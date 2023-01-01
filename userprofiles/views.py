9ab0f23a076212bf7e8d718138aada4d4da3383f
Madebybrown <madebybrown@gmail.com>
2023-01-01 14:48:13 +0000

    Add profile delete function to ProfileDetail class

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -35,3 +35,8 @@ class ProfileDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        profile = self.get_object(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
