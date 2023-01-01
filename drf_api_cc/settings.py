e8dce4b469ed72acd833e38205a8affea8ef3aed
Madebybrown <madebybrown@gmail.com>
2023-01-01 12:36:07 +0000

    Start project, add Profile app

--- drf_api_cc/settings.py
+++ drf_api_cc/settings.py
@@ -20,7 +20,7 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloduinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'userprofiles',
 ]
 
 MIDDLEWARE = [
